eaddc14e5c17f2baabb36863983ee7fb73c9e8e8
dimasekaputraaa <dimasteknik018@gmail.com>
2024-07-28 21:23:01 +0700

    add some fiture

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SetupData from "./components/setupData";
 import GoodReceive from "./components/goodReceive";
 import { ComboboxDemo } from "./components/autoComplete";
+import Chart from "./pages/chart";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />}>
-          <Route path="setup" element={<SetupData />} />
+          <Route path="setup" element={<Chart />} />
           <Route path="goodReceive" element={<GoodReceive />} />
           <Route path="Monitoring" element={<ComboboxDemo />} />
         </Route>
